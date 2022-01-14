da89056bf6e3e14e55d6b4cde96ca2867d3e43e6
Filippo Oleari <oleari.filippo@gmail.com>
2022-01-14 09:54:22 +0100

    Init

--- Prova/Program.cs
+++ Prova/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prova.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +10,14 @@ namespace Prova
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Prenotazioni Account Admin e Customer
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainView());
         }
     }
 }
--- Prova/View/MainView.Designer.cs
+++ Prova/View/MainView.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace Prova
+namespace Prova.View
 {
-    partial class Form1
+    partial class MainView
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace Prova
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "MainView";
         }
 
         #endregion
     }
 }
-
